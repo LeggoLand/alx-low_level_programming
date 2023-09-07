fbecd2fab4a3dfb8e799db328d3befd43cce78e0
LeggoLand <leesaygo.s@gmail.com>
2023-09-07 04:22:59 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/* This programs uses the puts function*/
+
+#include <stdio.h>
+
+int  main(void)
+{
+	/* Display results*/
+	puts("Programming is like building a multilingual puzzle:);
+	getchar();
+
+	return (0);
+}
