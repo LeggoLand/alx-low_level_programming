14e500599d256e3a4f248de242bd17b93fe1f5cc
LeggoLand <leesaygo.s@gmail.com>
2023-09-08 02:21:49 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	/* Print the result */
-	printf("with proper gramma, but the outcome is a piece of art, \n");
+	printf("with proper gramma, but the outcome is a piece of art,\n");
 
 	return (0);
 }
