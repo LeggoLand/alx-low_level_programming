b37b4ac39e092b040c9c97d45e4fa21e1a2efb04
LeggoLand <leesaygo.s@gmail.com>
2023-09-11 09:50:22 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabets in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowerAlpha;
+
+	for (lowerAlpha = 'z'; lowerAlpha <= 'a'; lowerAlpha--)
+	{
+		putchar(lowerAlpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
