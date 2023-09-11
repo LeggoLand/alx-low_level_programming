c2ab32193a278d072b534389de19dae4475e3256
LeggoLand <leesaygo.s@gmail.com>
2023-09-11 09:55:29 -0700

    alphabets in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char lowerAlpha;
 
-	for (lowerAlpha = 'z'; lowerAlpha <= 'a'; lowerAlpha--)
+	for (lowerAlpha = 'z'; lowerAlpha >= 'a'; lowerAlpha--)
 	{
 		putchar(lowerAlpha);
 	}
