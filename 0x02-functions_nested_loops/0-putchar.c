57719057eaec74d39932d680ea71b8ca297d64b7
LeggoLand <leesaygo.s@gmail.com>
2023-09-13 13:56:48 -0700

    nesting loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
-  * _putchar(char) - Prints _putchar
-  *
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point of the program
+ *
+ * Return: Always 0.
+ */
 
-int _putchar(char)
+int main(void)
 {
-	_putchar('_putchar');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
