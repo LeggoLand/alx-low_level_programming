197bccb276e8c20440215be7debd115239d5d3ac
LeggoLand <leesaygo.s@gmail.com>
2023-09-13 05:58:51 -0700

    variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar('0' + (i / 10));
 			putchar('0' + (i % 10));
@@ -21,7 +21,7 @@ int main(void)
 			putchar('0' + (j / 10));
 			putchar('0' + (j % 10));
 
-			if (i != 99 || j != 99)
+			if (i != 99 || j != 98)
 			{
 				putchar(',');
 				putchar(' ');
