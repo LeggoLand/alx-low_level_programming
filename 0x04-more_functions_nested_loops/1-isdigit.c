0f798b3e51580d60f9190e41f89dc9270b850993
LeggoLand <leesaygo.s@gmail.com>
2023-09-14 23:16:39 -0700

    more functions

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,6 +7,7 @@
  *
  * Return: 1 if c is digit, 0 otherwise.
  */
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
@@ -14,5 +15,6 @@ int _isdigit(int c)
 		return (1);
 	}
 
-	return (0);
+	else
+		return(0);
 }
